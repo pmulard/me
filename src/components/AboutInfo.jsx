0c6819cdc35d2f7ebbbf40d89d13195031b8b1cc
Peter Mulard <45550174+pmulard@users.noreply.github.com>
2022-01-27 17:28:04 -0800

    updating for current life info. move tree slightly to the left.

--- src/components/AboutInfo.jsx
+++ src/components/AboutInfo.jsx
@@ -17,20 +17,10 @@ class AboutInfo extends React.Component {
                 JBT Corp.
               </a>
             </span>
-            , a food manufacturing company, helping bring data from our industrial machines to the cloud. I also contribute to an open source project called&nbsp;
-            <span>
-              <a
-                className="work-link"
-                href="https://www.c0d3.com/"
-                target="blank"
-              >
-                c0d3.com
-              </a>
-            </span>
-            , helping students learn full stack software engineering.
+            , a food manufacturing company, helping bring data from our industrial machines to the cloud. Our projects have provided me a ton of knowledge for back-end microservices and cloud infrastructure, and it's been a really fun learning process so far.
           </p>
           <p>
-            Recently, I've been working with docker and linux, and enjoy using javascript in my free time. I also love to cook, backpack wilderness if I'm feeling adventerous, catch an occasional surf, and spend the rest of my time reading and learning.
+            Recently, I've been getting my hands dirty with Rust to get a better understanding of systems programming, which has been a great challenge. I also love to cook, backpack wilderness if I'm feeling adventerous, catch an occasional surf, and spend the rest of my time reading and learning.
           </p>
           <p>
             I hope to leave lasting, positive change by combining my career, skills and passions, while building some cool tech along the way. A few movements important to me - sustainability, criminal justice reform, and building a society that aligns with our values.
@@ -39,7 +29,7 @@ class AboutInfo extends React.Component {
         <hr />
         <div className="social-link-container" key="1">
           <a className="social-link" href="https://github.com/pmulard" target="blank">github</a>
-          <a className="social-link" href="https://twitter.com/pemmm12" target="blank">twitter</a>
+          <a className="social-link" href="https://twitter.com/petermulard" target="blank">twitter</a>
         </div>
       </div>
     );
