f0ee30245ef915bfc5a843abb0c01060c76cb65d
pmulard <peter.mulard@gmail.com>
2025-03-11 23:50:09 -0700

    update to my current life situation

--- src/components/AboutInfo.jsx
+++ src/components/AboutInfo.jsx
@@ -7,15 +7,18 @@ class AboutInfo extends React.Component {
       <div>
         <div className="text-container">
           <p>
-            Hi, I'm a software engineer with full stack experience, primarily with Rust, Python, JavaScript, and React.
-            I'm current building the backend for a next generation <a href="https://eth.nexandria.com" target="blank">blockchain explorer</a> at <a href="https://nexandria.com" target="blank">Nexandria</a> and am extremely passionate about what we do.
-            Feel free to contact me with any questions.
+            Hi, I'm a software engineer with full stack experience, primarily with Rust, Typescript, and React.
+            I'm current building a passion project of mine to bring motorcycle care into the 21st century with <a href="https://moto-memo.com" target="blank">MotoMemo</a>. 
+            Previously, I was working at <a href="https://nexandria.com" target="blank">Nexandria</a> to build a next generation EVM blockchain explorer.
+            Thank you Frederic, Omkar, and Coy for the learning opportunities and camaraderie - It accelerated my learning and confidence so much.
           </p>
           <p>
-            Recently, I've been programming almost exclusively with Rust and enjoy it more each day. I also love to cook, backpack wilderness if I'm feeling adventurous, catch an occasional surf, and spend the rest of my time reading and learning.
+            Recently, I've been programming almost exclusively with Rust and enjoy it more each day. 
+            I also love to cook, backpack wilderness if I'm feeling adventurous, ride my motorcycle, and spend the rest of my time reading and learning.
           </p>
           <p>
-            I hope to leave lasting, positive change by combining my career, skills and passions, while building some cool tech along the way. A few movements important to me - sustainability, criminal justice reform, and building a society that aligns with our values.
+            I hope to leave lasting, positive change by combining my career, skills and passions, while building some cool tech along the way. 
+            A few movements important to me - sustainability, criminal justice reform, and building a society that aligns with our values.
           </p>
         </div>
         <hr />
